a83a0fb4eb1837741cda4bc7711dd969b02526bb
srijan <srijan1214@gmail.com>
2019-05-28 09:19:38 -0400

    Trying this random push for learning.

--- app/src/main/java/com/example/screendestruction/MainActivity.java
+++ app/src/main/java/com/example/screendestruction/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ///asdasdasdasdasd
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
